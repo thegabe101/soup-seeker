dc9b872a6ee865316a278102fedf9a546892575f
Rayncor <sowa332@gmail.com>
2023-01-23 11:56:42 -0800

    trying to solve server issue

--- server/index.js
+++ server/index.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
+    port: 3306,
     password: "password",
     database: "LoginSystem",
+    debug: true,
 });
 
-app.listen(3001, () => {
+// db.connect(function (err) {
+//     if (err) console.error('error connecting ' + err.stack);
+//     console.log('Connected successfully.')
+// })
+
+app.post('/register', (req, res) => {
+
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query("INSERT INTO users(email, username, password) VALUES (?, ?, ?)", [email, username, password], (err, result) => {
+        console.log(result)
+        console.log(err);
+    })
+})
+
+app.listen(3306, () => {
     console.log('Server running.')
 })
