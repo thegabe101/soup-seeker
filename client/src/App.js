1f4958be432e9ec7549735be29631b9d2492f056
Rayncor <sowa332@gmail.com>
2023-01-24 14:59:47 -0800

    moved key functions to context

--- client/src/App.js
+++ client/src/App.js
@@ -19,9 +19,33 @@ function App() {
   const [currentGuess, setCurrentGuess] = useState({ attempt: 0, letterPosition: 0 });
   const [gamesWon, setGamesWon] = useState(0);
 
+  const onSelector = (keyValue) => {
+    //escape if clause to check if letter position is greater than 4 in the array; if so, exit because it needs to go to the next row 
+    if (currentGuess.letterPosition > 4) return;
+    const currentBoard = [...board];
+    currentBoard[currentGuess.attempt][currentGuess.letterPosition] = keyValue;
+    setBoard(currentBoard);
+    setCurrentGuess({ ...currentGuess, letterPosition: currentGuess.letterPosition + 1 })
+    console.log(currentBoard)
+  }
+
+  const onDelete = () => {
+    if (currentGuess.letterPosition === 0) return;
+    const currentBoard = [...board];
+    currentBoard[currentGuess.attempt][currentGuess.letterPosition - 1] = '';
+    setBoard(currentBoard);
+    setCurrentGuess({ ...currentGuess, letterPosition: currentGuess.letterPosition - 1 })
+  }
+
+  const onEnter = () => {
+    if (currentGuess.letterPosition < 5) return;
+    //increase the array index w attempt; reset position in array to start for next guess 
+    setCurrentGuess({ attempt: currentGuess.attempt + 1, letterPosition: 0 });
+  }
+
 
   return (
-    <AppContext.Provider value={{ board, setBoard, currentGuess, setCurrentGuess, gamesWon, setGamesWon }}>
+    <AppContext.Provider value={{ board, setBoard, currentGuess, setCurrentGuess, gamesWon, setGamesWon, onSelector, onDelete, onEnter }}>
       <AuthProvider>
         <div className="App">
           <Router>
