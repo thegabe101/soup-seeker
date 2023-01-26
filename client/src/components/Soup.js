73a7764134ada0a9f800e6832e60acbd4056fe58
Rayncor <sowa332@gmail.com>
2023-01-26 14:31:07 -0800

    tinkering with arrays

--- client/src/components/Soup.js
+++ client/src/components/Soup.js
@@ -2,15 +2,40 @@ import React from 'react';
 import "../App.css";
 import SoupImage from './SoupImage';
 import { useState } from 'react';
+import { useContext, useEffect } from 'react';
+import { AppContext } from '../App';
+import { soupTypes } from '../soup-types';
 
 
 function Soup(props) {
+    let i = 0;
+    const max = soupTypes.length;
+    const { soupIndex, setSoupIndex } = useContext(AppContext);
 
     // const { src, index } = props;
     // const [soupInfo, setSoupInfo] = useState({ src, index })
+    const genRandom = () => {
+        i = Math.random() * soupTypes.length;
+        // console.log(i);
+
+        i = Math.floor(i);
+        return i;
+    }
+
+
+    const indexSetter = () => {
+        genRandom();
+        setSoupIndex([Math.floor(Math.random(i) * max), Math.floor(Math.random(i) * max), Math.floor(Math.random(i) * max)]);
+        console.log(soupIndex);
+        console.log(max);
+    }
+
+    useEffect(() => {
+        indexSetter();
+    }, [])
 
     return (
-        <div><SoupImage  /></div>
+        <div><SoupImage index={soupIndex} /></div>
     )
 }
 
