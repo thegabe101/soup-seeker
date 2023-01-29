ee8b3fd13537a2f26eb4f3a0dc108e9d2f3746a4
Rayncor <sowa332@gmail.com>
2023-01-28 20:32:06 -0800

    tinkering w prof img

--- client/src/components/ImgUpload.js
+++ client/src/components/ImgUpload.js
@@ -1,10 +1,9 @@
-import "../App.css";
+// import "../App.css";
 import React, { Component } from "react";
 
 class ProfImg extends Component {
     constructor() {
         super();
-
         this.state = { avatar: "" };
     }
 
@@ -13,6 +12,7 @@ class ProfImg extends Component {
         reader.readAsDataURL(event.target.files[0]);
         reader.onload = (e) => {
             let img = e.target.result;
+            localStorage.setItem('profilepicture', img)
             this.setState({ avatar: img });
         };
     };
@@ -20,9 +20,9 @@ class ProfImg extends Component {
     render() {
         return (
             <div>
-                <input type="file" onChange={this.handleChange} />
+                {!localStorage.getItem('profilepicture') && <input type="file" onChange={this.handleChange} />}
                 <div>
-                    <img src={this.state.avatar} className="profImg" alt="" />
+                    {!localStorage.getItem('profilepicture') ? <img src={this.state.avatar} /> : <img src={localStorage.getItem('profilepicture')} alt="A profile picture." className="profImg" />}
                 </div>
             </div>
         );
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,6 +5,7 @@ import { useContext } from "react";
 import { AppContext } from "../App";
 import ProfImg from "../components/ImgUpload";
 import "../App.css";
+// import "../styles/profile.css";
 
 export const Profile = () => {
 	const { gamesWon, setGamesWon } = useContext(AppContext);
@@ -17,13 +18,20 @@ export const Profile = () => {
 	};
 
 	return (
-		<div>
-			<h1>Welcome, Seeker</h1>
-			<ProfImg />
-			<h2>Username: {auth.user}</h2>
-			<h2>You have won {gamesWon} games in your soup seeking career.</h2>
-			<div>
-				<button onClick={handleLogout}>Logout</button>
+		<div className="container">
+			<div className="card-wrapper">
+				<h1>Welcome, Seeker</h1>
+				<h2>
+					You have won {gamesWon} games in your soup seeking career.
+				</h2>
+				<div className="card">
+					<div className="card-image">
+						<ProfImg />
+					</div>
+					<div>
+						<h2 className="cardInfo">Username: {auth.user}</h2>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
