eb51645124f5d71af74296b2de9f5ba815fab510
Rayncor <sowa332@gmail.com>
2023-02-08 13:42:25 -0800

    minor render changes to landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -2,15 +2,31 @@ import React from "react";
 import { picasso } from "../assets/images/picassosoup.jpg";
 import "../styles/landing-page.css";
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "../App";
+import { useAuth } from "../components/AuthProvider";
 
 export const LandingPage = () => {
+	const auth = useAuth();
+
 	return (
 		<div className="picasso">
 			<div className="seek">
-			<h1>Welcome, Soup Seeker</h1>
-				<NavLink to="/login" className="button-5">
-					Login
-				</NavLink>
+				{auth.user ? (
+					<>
+						<h1>Welcome, Soup Seeker</h1>{" "}
+						<NavLink to="/play" className="button-5">
+							Play
+						</NavLink>{" "}
+					</>
+				) : (
+					<>
+						<h1>Welcome, Soup Seeker</h1>
+						<NavLink to="/login" className="button-5">
+							Login
+						</NavLink>
+					</>
+				)}
 			</div>
 		</div>
 	);
